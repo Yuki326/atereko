b583f2f88813052442178d0295af1a6d85a45b65
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-09-21 01:35:13 +0900

    fix env

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ import { getDatabase, ref, onValue } from 'firebase/database'
 
 import { Top } from './components/Top'
 import { Login } from './components/Login'
-import { configs } from './config.json'
-import './App.css';
+import { 
+  apiKey, 
+  authDomain, 
+  fdbUrl, 
+  projectId, 
+  storageBucket, 
+  messageSenderId, 
+  appId 
+} from './config.json'
+import './App.css'
 
-initializeApp(configs)
+const firebaseConfig = {
+  "apiKey": process.env.REACT_APP_API_KEY || apiKey,
+  "authDomain": process.env.REACT_APP_AUTH_DOMAIN || authDomain,
+  "databaseURL": process.env.REACT_APP_FDB_URL || fdbUrl,
+  "projectId": process.env.REACT_APP_PROJECT_ID || projectId,
+  "storageBucket": process.env.REACT_APP_STORAGE_BUCKET || storageBucket,
+  "messagingSenderId": process.env.REACT_APP_MESSAGE_SENDER_ID || messageSenderId,
+  "appId": process.env.REACT_APP_APP_ID || appId 
+};
+console.log('config', firebaseConfig)
+
+initializeApp(firebaseConfig)
 
 const formatImgs = (imgs) => {
   const formatedImgs = Object.keys(imgs).map(key => imgs[key])
