fba58275979e04a47ec43f8dc44c325841a22d76
Yuki326 <88820769+Yuki326@users.noreply.github.com>
2021-09-20 09:02:33 +0900

    ページ全体のレイアウトを作成

--- src/App.js
+++ src/App.js
@@ -5,6 +5,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/*仮のレイアウト*/}
+        <div class = "container">
+          <div class = "header">
+            <div class = "navbar">
+              <div class = "logo">
+                ロゴ
+              </div>
+              <div class = "grobal-nav">
+                <ul>
+                  <li class = "search"><a href = "#">検索窓</a></li>
+                  <li class = "login"><a href ="#">ログイン</a></li>
+                  <li class = "prof"><a href = "#">プロフィール</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class = "main">
+            <div class = "contents">
+              <p>contents<img src={logo} className="App-logo" alt="logo" /></p>
+            </div>
+            <div class = "sidebar">
+              <h2>最新の動画</h2>
+              {/*データベースから持ってくる*/}
+              <div class = "section">
+                <div class = "img">サムネ</div>
+                <div class = "desc">説明</div>
+              </div>
+              <div class = "section">
+                <div class = "img">サムネ</div>
+                <div class = "desc">説明</div>
+              </div>
+            </div>
+          </div>
+          <div class = "footer">footer</div>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>テスト</code> and save to reload.
