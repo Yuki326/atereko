a41048120f5681c978f093b17ecf191a1cdbfe94
Yuki326 <88820769+Yuki326@users.noreply.github.com>
2021-09-20 13:31:00 +0900

    コンフリクト解消

--- src/App.js
+++ src/App.js
@@ -40,20 +40,7 @@ function App() {
           </div>
           <div class = "footer">footer</div>
         </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>テスト</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     </header>
     </div>
   );
 }
