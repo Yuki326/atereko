b9eb327b267f985db3b3f28bf22d43fe0651811a
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-09-20 14:15:30 +0900

    fix-style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*仮のレイアウト*/}
         <div class = "container">
           <div class = "header">
             <div class = "navbar">
@@ -14,20 +12,19 @@ function App() {
               </div>
               <div class = "grobal-nav">
                 <ul>
-                  <li class = "search"><a href = "#">検索窓</a></li>
-                  <li class = "login"><a href ="#">ログインa</a></li>
-                  <li class = "prof"><a href = "#">プロフィール</a></li>
+                  <li class = "search"><a href = "/#">検索窓</a></li>
+                  <li class = "login"><a href ="/#">ログインa</a></li>
+                  <li class = "prof"><a href = "/#">プロフィール</a></li>
                 </ul>
               </div>
             </div>
           </div>
           <div class = "main">
             <div class = "contents">
-              <p>contents<img src={logo} className="App-logo" alt="logo" /></p>
+              
             </div>
             <div class = "sidebar">
               <h2>最新の動画</h2>
-              {/*データベースから持ってくる*/}
               <div class = "section">
                 <div class = "img">サムネ</div>
                 <div class = "desc">説明</div>
@@ -41,7 +38,6 @@ function App() {
           <div class = "footer">footer</div>
         </div>
 
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>テスト</code> and save to reload.
         </p>
