d7e0e86a0e990dd9e8c9072bcee14725b6f269ec
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-09-21 22:36:39 +0900

    fix loading display

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react'
-import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { getDatabase, ref, onValue } from 'firebase/database'
 
 const formatImgs = (imgs) => {
@@ -8,12 +9,6 @@ const formatImgs = (imgs) => {
   const formatedImgs = Object.keys(imgs).map(key => imgs[key])
   return formatedImgs
 }
-
-/*
-ランダムにアカウントを選ぶ
-そのアカウントでログインする
-アカウントの画像取得
-*/
 
 const createPassword = (account) => {
   const words = account.split('@') 
@@ -27,15 +22,21 @@ export const Top = ({ acts, setRoute }) => {
 
   const [accounts, setAccounts] = useState(acts)
   const [currentAccount, setCurrentAccount] = useState()
-  const [loginError, setLoginError] = useState(null)
-  const [isLoadingLogin, setIsLoadingLogin] = useState(false)
   const [uid, setUid] = useState()
   const [imgs, setImgs] = useState(null)
+
+  const [isLoadingLogin, setIsLoadingLogin] = useState(false)
+  const [loginError, setLoginError] = useState(null)
+  const [isLoadingGetImgs, setIsLoadingGetImgs] = useState(false)
+  // const [getImgsError, setGetImgsError] = useState()
   // console.log('accounts', accounts)
+
 
   const chooseAccount = () => {
     if (accounts.length <= 0) {
       alert('全てのアカウントが選ばれました')
+      localStorage.clear()
+      setRoute('entry')
       return
     }
     const randomId = Math.floor(Math.random() * accounts.length)
@@ -54,6 +55,7 @@ export const Top = ({ acts, setRoute }) => {
 
     } catch (error) {
       console.log('loginError', error)
+      setIsLoadingLogin(false)
       setLoginError(error)
     }
   }
@@ -68,23 +70,26 @@ export const Top = ({ acts, setRoute }) => {
 
   useEffect(() => {
     if (uid) {
+      setIsLoadingGetImgs(true)
       const userImgsRef = ref(db, `users/${uid}`);
       onValue(userImgsRef, (snapshot) => {
           const data = snapshot.val()
           setImgs(formatImgs(data))
-        })
+      })
+      setIsLoadingGetImgs(false)
     }
   }, [uid])
 
   if (loginError) return <div>Login Error Happened.</div>
-
+  if (isLoadingGetImgs) return <div>Now Get Images...</div>
+  if (isLoadingLogin) return <div>Now Login...</div>
 
   return (
     <>
      <div>{`現在のアカウント: ${currentAccount ? currentAccount : "選択されていません"}`}</div>
      <button onClick={chooseAccount}>抽選</button>
-     {!currentAccount ? null : isLoadingLogin ? <div>Now Login...</div> : <div>Login Success.</div>}
-     {imgs && imgs.length > 0 ? imgs.map(ref => <div key={ref.url}><img alt="夏休み中の画像" src={ref.url} height="200" width="200" /></div>) : null}
+     {/* {isLoadingLogin ? <div>Now Login...</div> : isLoadingGetImgs ? <div>Now Get Images...</div> : null} */}
+     {imgs && imgs.length > 0 ? imgs.map(ref => <div key={ref.url}><img alt="夏休み中の画像" src={ref.url} height="200" width="200" /></div>) : <div>保存されている画像はありません。</div>}
     </>
   )
 
