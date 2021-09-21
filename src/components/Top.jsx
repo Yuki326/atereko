67743fae1a564dcf16782978d8a117ab36b19d39
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-09-22 01:14:51 +0900

    add hint function

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -25,12 +25,36 @@ export const Top = ({ acts, setRoute }) => {
   const [uid, setUid] = useState()
   const [imgs, setImgs] = useState(null)
 
+  const [hintImgs, setHintImgs] = useState(null)
+  const [hintCount, setHintCount] = useState(1)
+  const [isHintFull, setIsHintFull] = useState(false)
+
   const [isLoadingLogin, setIsLoadingLogin] = useState(false)
   const [loginError, setLoginError] = useState(null)
   const [isLoadingGetImgs, setIsLoadingGetImgs] = useState(false)
   // const [getImgsError, setGetImgsError] = useState()
   // console.log('accounts', accounts)
 
+  const handleHint = () => {
+    if (imgs) {
+      const nextHintCount = hintCount + 1
+      if (nextHintCount === imgs.length) {
+        setIsHintFull(true)
+      }
+      setHintCount(prev => prev + 1)
+    }
+  }
+  
+  const handleShowAll = () => {
+    setHintImgs(imgs)
+    setIsHintFull(true)
+  }
+
+  const clearHint = () => {
+    setIsHintFull(false)
+    setHintCount(1)
+    setHintImgs(imgs.slice(0, hintCount))
+  }
 
   const chooseAccount = () => {
     if (accounts.length <= 0) {
@@ -80,6 +104,23 @@ export const Top = ({ acts, setRoute }) => {
     }
   }, [uid])
 
+  useEffect(() => {
+    if (imgs) {
+      console.log('change imgs', imgs)
+      setIsHintFull(false)
+      setHintCount(1)
+      setHintImgs(imgs.slice(0, hintCount))
+    }
+  }, [imgs])
+
+  useEffect(() => {
+   if (imgs) {
+     console.log('change hint', imgs)
+     setHintImgs(imgs.slice(0, hintCount))
+   }
+
+  }, [hintCount])
+
   if (loginError) return <div>Login Error Happened.</div>
   if (isLoadingGetImgs) return <div>Now Get Images...</div>
   if (isLoadingLogin) return <div>Now Login...</div>
@@ -89,7 +130,10 @@ export const Top = ({ acts, setRoute }) => {
      <div>{`現在のアカウント: ${currentAccount ? currentAccount : "選択されていません"}`}</div>
      <button onClick={chooseAccount}>抽選</button>
      {/* {isLoadingLogin ? <div>Now Login...</div> : isLoadingGetImgs ? <div>Now Get Images...</div> : null} */}
-     {imgs && imgs.length > 0 ? imgs.map(ref => <div key={ref.url}><img alt="夏休み中の画像" src={ref.url} height="200" width="200" /></div>) : <div>保存されている画像はありません。</div>}
+     {hintImgs && hintImgs.length > 0 ? hintImgs.map(ref => <div key={ref.url}><img alt="夏休み中の画像" src={ref.url} height="200" width="200" /></div>) : null}
+     {!isHintFull && <button onClick={handleHint}>ヒント</button>}
+     {!isHintFull && <button onClick={handleShowAll}>すべて</button>}
+     {isHintFull && <button onClick={clearHint}>クリア</button>}
     </>
   )
 
